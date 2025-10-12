8124d7715554e4eac4a23313949b64623331f408
Matej Alexej Helc <matej.helc1@gmail.com>
2025-10-12 10:48:54 +0200

    Feat: Add remaining controllers

---
+++ DataAccessLayer/Repository/PublisherRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Context;
+using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repository;
+
+public class PublisherRepository : BaseRepository<Publisher>
+{
+    public PublisherRepository(BookHubDbContext context) : base(context)
+    {
+    }
+}
---
+++ WebAPI/Controllers/ImageController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("image")]
+[ApiController]
+public class ImageController : BaseController<Image>
+{
+    public ImageController(IRepository<Image> repository) : base(repository)
+    {
+
+    }
+
+    [HttpGet]
+    public Task<IActionResult> GetAllImages() => GetAll();
+
+    [HttpGet("{id}")]
+    public Task<IActionResult> GetImage(int id) => Get(id);
+
+    [HttpPost]
+    public Task<IActionResult> CreateImage([FromBody] Image entity) => Insert(entity);
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> UpdateImage(int id, [FromBody] Image entity) => Update(id, entity);
+
+    [HttpDelete("{id}")]
+    public Task<IActionResult> DeleteImage(int id) => Delete(id);
+}
