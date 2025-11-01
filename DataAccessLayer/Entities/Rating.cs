87a41fa5a463a5e2c4aac3f28f2bad3938c8a9da
Ján Špaček <xspacek1@fi.muni.cz>
2025-11-01 20:34:59 +0100

    chore: consistent code style for property definitions

--- DataAccessLayer/Entities/Rating.cs
+++ DataAccessLayer/Entities/Rating.cs
@@ -6,7 +6,8 @@ namespace DataAccessLayer.Entities;
 public class Rating : BaseEntity
 {
     [Required]
-    [Range(0, 5)] public required int Stars { get; set; }
+    [Range(0, 5)] 
+    public required int Stars { get; set; }
 
     [ForeignKey(nameof(UserId))]
     public virtual User User { get; set; }
